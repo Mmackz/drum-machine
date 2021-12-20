a47d811d2c0430882f9e45f72ab169c4069acf0d
Mmackz <matthewmackenz@gmail.com>
2021-12-20 14:54:22 -0800

    📐  layout basic structure

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
+import { useState } from "react";
 import Controls from "./components/Controls";
-import Keypad from "./components/Keypad"
+import Keypad from "./components/Keypad";
 import { bankOne, bankTwo } from "./drum-sounds";
 
 function App() {
+   const [options, setOptions] = useState({
+      powerOn: false,
+      sound: "",
+      volume: 30,
+      bank: 0,
+   });
+
    return (
       <div className="container">
          {/* {fcc: icon} */}
+         <div className="icon-container">
+            <span className="icon-text">FCC</span>
+            <i className="icon fab fa-free-code-camp"></i>
+         </div>
+
          {/* {keypad} */}
          <Keypad />
          {/* {control center} */}
-         <Controls />
+         <Controls options={options} setOptions={setOptions} />
       </div>
    );
 }
