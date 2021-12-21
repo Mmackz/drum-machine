0d318d041741ed989f52d25a79f6caf12de7d954
Mmackz <matthewmackenz@gmail.com>
2021-12-20 20:31:18 -0800

    🎨 basic layout and styling ☑

--- src/components/app/styles/app.js
+++ src/components/app/styles/app.js
@@ -2,8 +2,15 @@ import styled from "styled-components";
 
 export const Container = styled.main`
    border: 4px solid orange;
+   padding: 1rem;
    position: relative;
    background: #b3b3b3;
+   display: flex;
+   flex-direction: column;
+
+   @media (min-width: 670px) {
+      flex-direction: row;
+   }
 `;
 
 export const Icon = styled.i`
@@ -15,6 +22,7 @@ export const Icon = styled.i`
 
 export const IconContainer = styled.div`
    position: absolute;
+   top: 0;
    right: 6px;
    font-style: italic;
 `;
