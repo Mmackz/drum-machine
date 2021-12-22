22fabf215ace8da36ff66669991cdd6c47123a33
Mmackz <matthewmackenz@gmail.com>
2021-12-21 17:55:33 -0800

    ✨ connect keys to state

--- src/components/key/index.js
+++ src/components/key/index.js
@@ -1,7 +1,19 @@
 import { Button } from "./styles/key";
 
-function Key({ data }) {
-   return <Button>{String.fromCharCode(data.keyCode)}</Button>;
+function Key({ data, state }) {
+   const { powerOn, volume } = state.options;
+
+   function playSound() {
+      if (powerOn) {
+         const sound = new Audio(data.url);
+         sound.volume = volume / 100;
+         sound.play();
+      }
+   }
+
+   const keyTrigger = String.fromCharCode(data.keyCode);
+
+   return <Button onClick={playSound}>{keyTrigger}</Button>;
 }
 
 export default Key;
--- src/components/keypad/index.js
+++ src/components/keypad/index.js
@@ -2,12 +2,12 @@ import Key from "../key";
 import { Pad } from "./styles/keypad";
 
 function Keypad(props) {
-   const { bank } = props;
+   const { bank, state } = props;
 
    return (
       <Pad>
          {bank.map((item) => (
-            <Key key={item.keyCode} data={item} />
+            <Key key={item.keyCode} data={item} state={state} />
          ))}
       </Pad>
    );
