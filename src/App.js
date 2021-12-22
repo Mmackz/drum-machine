eb455145c440d99ac96eaf6b9c0251328c068941
Mmackz <matthewmackenz@gmail.com>
2021-12-21 18:15:15 -0800

    🔥 remove hyphens in instrument names

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Controls from "./components/controls";
 import Keypad from "./components/keypad";
 import Logo from "./components/logo";
 import { Container } from "./components/app/styles/app";
-import { bank_0, bank_1 } from "./media/drum-sounds";
+import { bank_0, bank_1 } from "./data/drum-sounds";
 
 function App() {
    const [options, setOptions] = useState({
