4d42b0bcd82e246d66116d979aa5f0f6d54a46b1
Mmackz <matthewmackenz@gmail.com>
2021-12-21 17:06:59 -0800

    🎨 add media queries + minor layout adjustments

--- src/components/app/styles/app.js
+++ src/components/app/styles/app.js
@@ -2,13 +2,18 @@ import styled from "styled-components";
 
 export const Container = styled.main`
    border: 4px solid orange;
-   padding: 1rem;
+   padding: 1.25rem;
+   min-width: 340px;
+   width: 65vw;
    position: relative;
    background: #b3b3b3;
    display: flex;
    flex-direction: column;
+   gap: 2.5rem;
 
    @media (min-width: 670px) {
       flex-direction: row;
+      width: 660px;
+      gap: 1.3rem;
    }
 `;
